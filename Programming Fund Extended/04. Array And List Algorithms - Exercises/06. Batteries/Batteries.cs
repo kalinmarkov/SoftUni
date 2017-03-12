16891aaa726b8ef742aa83c2e697278040757fa4
Калин Марков <kalin_markoff@mail.bg>
2017-03-12 18:04:19 +0200

    Added new solutions

--- Programming Fund Extended/04. Array And List Algorithms - Exercises/06. Batteries/Batteries.cs
+++ Programming Fund Extended/04. Array And List Algorithms - Exercises/06. Batteries/Batteries.cs
@@ -1,14 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 class Batteries
 {
     static void Main()
     {
+        var batteriesCapacities = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        var usagePerHour = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        var hoursToTest = int.Parse(Console.ReadLine());
 
+        var result = new StringBuilder();
+
+        for (int i = 0; i < batteriesCapacities.Length; i++)
+        {
+            var capacity = batteriesCapacities[i];
+            var batteryUsagePerHour = usagePerHour[i];
+
+            var mAh = capacity - (batteryUsagePerHour * hoursToTest);
+
+            if (mAh > 0)
+            {
+                result.AppendLine($"Battery {i + 1}: {mAh:f2} mAh ({(mAh * 100.0) / capacity:f2})%");
+            }
+            else
+            {
+
+                result.AppendLine($"Battery {i + 1}: dead (lasted {Math.Ceiling(capacity / batteryUsagePerHour)} hours)");
+            }
+        }
+
+        Console.Write(result);
     }
 }
 
