1950585cf9282dbf5d714b9a0e26a03b42753c76
Калин Марков <kalin_markoff@mail.bg>
2017-03-13 21:30:46 +0200

    Added new solution

---
+++ Programming Fund Extended/05. Array and List Algorithms - More Exercises/02. Japanese Roulette/JapaneseRoulette.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class JapaneseRoulette
+{
+    static void Main()
+    {
+        var cylinder = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var players = Console.ReadLine().Split(' ').ToList();
+
+        var firstBulletPosition = cylinder.IndexOf(1);        
+        var lastPositionBullet = 0;
+        var isDeath = false;
+
+        for (int i = 0; i < players.Count; i++)
+        {
+            var commands = players[i].Split(',').ToArray();
+            var strengthOfRotation = int.Parse(commands[0]);
+            var direction = commands[1];
+
+            if (direction == "Right")
+            {
+                lastPositionBullet = (firstBulletPosition + strengthOfRotation) % cylinder.Count;
+                firstBulletPosition = lastPositionBullet;
+            }
+            else
+            {
+                lastPositionBullet = (firstBulletPosition - strengthOfRotation) % cylinder.Count;
+                if (lastPositionBullet < 0)
+                {
+                    lastPositionBullet += cylinder.Count;
+                }
+                firstBulletPosition = lastPositionBullet;
+                
+            }
+
+            if (lastPositionBullet == 2)
+            {
+                Console.WriteLine($"Game over! Player {i} is dead.");
+                isDeath = true;
+                break;
+            }
+            firstBulletPosition++;
+        }
+
+        if (!isDeath)
+        {
+            Console.WriteLine("Everybody got lucky!");
+        }
+
+    }
+}
+
---
+++ Programming Fund Extended/05. Array and List Algorithms - More Exercises/03. Increasing Crisis/IncreasingCrisis.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class IncreasingCrisis
+{
+    static void Main()
+    {
+        // 60/100
+
+        int n = int.Parse(Console.ReadLine());
+
+        var listOfNumbers = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int mostRightElement = 0;
+            int index = 0;
+            bool isBreak = false;
+
+            if (i == 0)
+            {                
+                listOfNumbers.AddRange(input);                
+            }
+            else
+            {
+                for (int j = 0; j < input.Count; j++)
+                {
+                    for (int k = 0; k < listOfNumbers.Count; k++)
+                    {
+                        if (input[j] > listOfNumbers[k])
+                        {
+                            mostRightElement = listOfNumbers[k];
+                            index = listOfNumbers.IndexOf(mostRightElement);
+                        }
+                        else if (input[j] == listOfNumbers[k])
+                        {
+                            index++;
+                        }
+                    }
+                    break;
+                }
+
+                listOfNumbers.InsertRange(index + 1, input);
+            }
+
+            int breakElement = 0;
+            int breakPostion = 0;
+
+            for (int j = 1; j < listOfNumbers.Count; j++)
+            {
+                if (listOfNumbers[j] < listOfNumbers[j - 1])
+                {
+                    breakElement = listOfNumbers[j];
+                    breakPostion = listOfNumbers.LastIndexOf(breakElement);
+                    isBreak = true;
+                    break;
+                }
+            }
+
+            if (isBreak)
+            {
+                for (int j = listOfNumbers.Count - 1; j >= breakPostion; j--)
+                {
+                    listOfNumbers.RemoveAt(j);
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", listOfNumbers));
+
+    }
+}
+
