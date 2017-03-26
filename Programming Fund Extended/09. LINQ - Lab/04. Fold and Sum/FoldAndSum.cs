1ccab6c07221a47eaa59412638dc9781eba100f1
Калин Марков <kalin_markoff@mail.bg>
2017-03-26 17:59:14 +0300

    Added new solutions in HTML & CSS

--- Programming Fund Extended/09. LINQ - Lab/04. Fold and Sum/FoldAndSum.cs
+++ Programming Fund Extended/09. LINQ - Lab/04. Fold and Sum/FoldAndSum.cs
@@ -1,14 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class FoldAndSum
 {
     static void Main()
     {
+        var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var k = input.Length / 4;
+        var leftPart = input.Take(k).Reverse().ToList();
+        var rightPart = input.Reverse().Take(k).ToList();
 
+        var upperRow = leftPart.Concat(rightPart).ToArray();
+        var lowerRow = input.Skip(k).Take(2 * k).ToArray();
+
+        var result = new int[lowerRow.Length];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = upperRow[i] + lowerRow[i];
+        }
+
+        Console.WriteLine(string.Join(" ", result));
     }
 }
 
