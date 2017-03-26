3a6fffc1345198aba1e53496c2ebf71fe464f863
Калин Марков <kalin_markoff@mail.bg>
2017-03-26 19:08:07 +0300

    Added more exercises with Lambda and LINQ

--- Programming Fund Extended/11. Lambda and LINQ - More Exercises/01. Lambada Expressions/LambadaExpressions.cs
+++ Programming Fund Extended/11. Lambda and LINQ - More Exercises/01. Lambada Expressions/LambadaExpressions.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _01.Lambada_Expressions
+class LambadaExpressions
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        var lambdaStorage = new Dictionary<string, Dictionary<string, string>>();
+
+        while (true)
         {
+            var input = Console.ReadLine();
+            if (input == "lambada")
+            {
+                break;
+            }
+
+            var tokens = input.Split(new[] { ' ', '.', '=', '>' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            if (tokens.Length > 1)
+            {
+                var selector = tokens[0];
+                var selectorObject = tokens[1];
+                var property = tokens[2];
+
+                if (!lambdaStorage.ContainsKey(selector))
+                {
+                    lambdaStorage[selector] = new Dictionary<string, string>();
+                }
+
+                lambdaStorage[selector][selectorObject] = property;
+            }
+            else
+            {
+                lambdaStorage = lambdaStorage
+                    .ToDictionary(selector => selector.Key, 
+                    selector => selector.Value.ToDictionary(selectorObj => selectorObj.Key, 
+                    selectorObj => selectorObj.Key + '.' + selectorObj.Value));
+            }
+            
+        }
+
+        foreach (var selector in lambdaStorage)
+        {
+            foreach (var item in selector.Value)
+            {
+                var selectorObj = item.Key;
+                var property = item.Value;
+                Console.WriteLine($"{selector.Key} => {selectorObj}.{property}");
+            }
+
         }
     }
 }
+
+
