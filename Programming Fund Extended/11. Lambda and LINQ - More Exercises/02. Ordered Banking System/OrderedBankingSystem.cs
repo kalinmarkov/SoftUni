f2bbf8f827e4f8e4e6693fdb71691e659db1879e
Калин Марков <kalin_markoff@mail.bg>
2017-03-28 19:41:35 +0300

    solved problems with OrderedBankingSystem solution

--- Programming Fund Extended/11. Lambda and LINQ - More Exercises/02. Ordered Banking System/OrderedBankingSystem.cs
+++ Programming Fund Extended/11. Lambda and LINQ - More Exercises/02. Ordered Banking System/OrderedBankingSystem.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class OrderedBankingSystem
 {
     static void Main()
     {
+        var bankDatabase = new Dictionary<string, Dictionary<string, decimal>>();
 
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "end")
+            {
+                break;
+            }
+
+            var tokens = input.Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var bankName = tokens[0];
+            var account = tokens[1];
+            var balance = decimal.Parse(tokens[2]);
+
+            if (!bankDatabase.ContainsKey(bankName))
+            {
+                bankDatabase[bankName] = new Dictionary<string, decimal>();
+            }
+
+            if (!bankDatabase[bankName].ContainsKey(account))
+            {
+                bankDatabase[bankName][account] = 0;
+            }
+
+            bankDatabase[bankName][account] += balance;
+        }
+
+        foreach (var bank in bankDatabase.OrderByDescending(bank => bank.Value.Sum(account => account.Value))
+                                        .ThenByDescending(bank => bank.Value.Max(account => account.Value)))
+        {
+            var bankName = bank.Key;
+            foreach (var bankAccount in bank.Value.OrderByDescending(account => account.Value))
+            {
+                var account = bankAccount.Key;
+                var balance = bankAccount.Value;
+                Console.WriteLine($"{account} -> {balance} ({bankName})");
+            }
+        }
     }
 }
-
