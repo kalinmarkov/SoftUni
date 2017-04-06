6cec54bddf7999ba798e97bda67427ba65fa3e6d
Калин Марков <kalin_markoff@mail.bg>
2017-04-06 18:11:14 +0300

    Java SoftUni Blog

---
+++ Software Technologies/09. Java SoftUni Blog/blog/src/main/java/softuniBlog/controller/ArticleController.java
@@ -0,0 +1,71 @@
+package softuniBlog.controller;
+
+import com.sun.javafx.sg.prism.NGShape;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import softuniBlog.bindingModel.ArticleBindingModel;
+import softuniBlog.entity.Article;
+import softuniBlog.entity.User;
+import softuniBlog.repository.ArticleRepository;
+import softuniBlog.repository.UserRepository;
+
+@Controller
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/article/create")
+    @PreAuthorize("isAuthenticated()")
+    public String create(Model model) {
+        model.addAttribute("view", "article/create");
+
+        return "base-layout";
+    }
+
+    @PostMapping("/article/create")
+    @PostAuthorize("isAuthenticated()")
+    public String createProcess(ArticleBindingModel model) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        User user = this.userRepository.findByEmail(userDetails.getUsername());
+
+        Article article = new Article(
+                model.getTitle(),
+                model.getContent(),
+                user
+        );
+
+        this.articleRepository.saveAndFlush(article);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/article/{id}")
+    public String details(Model model, @PathVariable Integer id) {
+        if(!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+
+        Article article = this.articleRepository.findOne(id);
+
+        model.addAttribute("article", article);
+        model.addAttribute("view", "article/details");
+
+        return "base-layout";
+    }
+}
