4442cdc1b23672f38a6747e091906799d7525402
Калин Марков <kalin_markoff@mail.bg>
2017-04-08 19:17:36 +0300

    classes and objects solutions

---
+++ Programming Fund Extended/13. Objects and Simple Classes - More Exercises/02. Optimized Banking System/OptimizedBankingSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BankAccount
+{
+    public string BankName { get; set; }
+
+    public string AccountName { get; set; }
+
+    public List<decimal> Balance { get; set; }
+}
+
+class OptimizedBankingSystem
+{
+    static void Main()
+    {
+        //  60/100
+
+        var bankAccounts = new List<BankAccount>();
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "end")
+            {
+                break;
+            }
+
+            var tokens = input.Split(" |".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var bankName = tokens[0];
+            var accountName = tokens[1];
+            var balance = new List<decimal>();
+            balance.Add(decimal.Parse(tokens[2]));
+
+            var newBankAccount = new BankAccount()
+            {
+                BankName = bankName,
+                AccountName = accountName,
+                Balance = balance
+            };
+
+            bankAccounts.Add(newBankAccount);
+        }
+
+        foreach (var bank in bankAccounts.OrderByDescending(x => x.Balance.Sum()))
+        {
+            var accountName = bank.AccountName;
+            var bankName = bank.BankName;
+            var balance = bank.Balance;
+
+            Console.WriteLine($"{accountName} -> {balance} ({bankName})");
+        }
+    }
+}
+
---
+++ Programming Fund Extended/13. Objects and Simple Classes - More Exercises/03. Animals/Animals.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Dog
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public int NumberOfLegs { get; set; }
+
+    public void ProduceSound()
+    {
+        Console.WriteLine("I'm a Distinguishedog, and I will now produce a distinguished sound! Bau Bau.");
+    }
+}
+
+class Cat
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public int IntelligenceQuotient { get; set; }
+
+    public void ProduceSound()
+    {
+        Console.WriteLine("I'm an Aristocat, and I will now produce an aristocratic sound! Myau Myau.");
+    }
+}
+
+class Snake
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public int CrueltyCoefficient { get; set; }
+
+    public void ProduceSound()
+    {
+        Console.WriteLine("I'm a Sophistisnake, and I will now produce a sophisticated sound! Honey, I'm home.");
+    }
+}
+
+class Animals
+{
+    static void Main()
+    {
+        var dogs = new Dictionary<string, Dog>();
+        var cats = new Dictionary<string, Cat>();
+        var snakes = new Dictionary<string, Snake>();
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "I'm your Huckleberry")
+            {
+                break;
+            }
+
+            var tokens = input.Split();
+
+            if (tokens[0] != "talk")
+            {
+                var classAnimal = tokens[0];
+                var name = tokens[1];
+                var age = int.Parse(tokens[2]);
+                var parameter = int.Parse(tokens[3]);
+
+                switch (classAnimal)
+                {
+                    case "Dog":
+                        var newDog = new Dog()
+                        {
+                            Name = name,
+                            Age = age,
+                            NumberOfLegs = parameter
+                        };
+
+                        dogs.Add(newDog.Name, newDog);
+                        break;
+                    case "Cat":
+                        var newCat = new Cat()
+                        {
+                            Name = name,
+                            Age = age,
+                            IntelligenceQuotient = parameter
+                        };
+
+                        cats.Add(newCat.Name, newCat);
+                        break;
+                    case "Snake":
+                        var newSnake = new Snake()
+                        {
+                            Name = name,
+                            Age = age,
+                            CrueltyCoefficient = parameter
+                        };
+
+                        snakes.Add(newSnake.Name, newSnake);
+                        break;
+                }
+            }
+            else
+            {
+                var animalName = tokens[1];
+
+                if (dogs.ContainsKey(animalName))
+                {
+                    dogs[animalName].ProduceSound();
+                }
+                else if (cats.ContainsKey(animalName))
+                {
+                    cats[animalName].ProduceSound();
+                }
+                else if (snakes.ContainsKey(animalName))
+                {
+                    snakes[animalName].ProduceSound();
+                }
+            }
+            
+        }
+
+        foreach (var dog in dogs.Values)
+        {
+            var name = dog.Name;
+            var age = dog.Age;
+            var numberOfLegs = dog.NumberOfLegs;
+            Console.WriteLine($"Dog: {name}, Age: {age}, Number Of Legs: {numberOfLegs}");
+        }
+
+        foreach (var cat in cats.Values)
+        {
+            var name = cat.Name;
+            var age = cat.Age;
+            var intelligenceQontient = cat.IntelligenceQuotient;
+            Console.WriteLine($"Cat: {name}, Age: {age}, IQ: {intelligenceQontient}");
+        }
+
+        foreach (var snake in snakes.Values)
+        {
+            var name = snake.Name;
+            var age = snake.Age;
+            var crueltyCoefficient = snake.CrueltyCoefficient;
+            Console.WriteLine($"Snake: {name}, Age: {age}, Cruelty: {crueltyCoefficient}");
+        }
+    }
+}
+
