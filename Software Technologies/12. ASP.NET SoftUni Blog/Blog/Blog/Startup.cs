487f73c16e666a4e5c407719a66c0b1ecb46baad
Калин Марков <kalin_markoff@mail.bg>
2017-04-19 22:20:43 +0300

    Added ASP.NET Blog - Basic Functionality

---
+++ Software Technologies/12. ASP.NET SoftUni Blog/Blog/Blog/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+using Blog.Models;
+using Blog.Migrations;
+
+[assembly: OwinStartupAttribute(typeof(Blog.Startup))]
+namespace Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+
+            ConfigureAuth(app);
+        }
+    }
+}
