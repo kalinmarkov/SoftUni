ba7a38ae355a1796256bb6ca90bf6caa27fbaa49
Калин Марков <kalin_markoff@mail.bg>
2017-04-19 18:31:42 +0300

    Added C# Calculator

---
+++ Software Technologies/11. C# Calculator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+
+        public decimal LeftOperand { get; set; }
+
+        public decimal RightOperand { get; set; }
+
+        public string Operator { get; set; }
+
+        public decimal Result { get; set; }
+
+        
+    }
+}
