4bbc0e364b865028893d9452d9d5bfc252ee1f22
Калин Марков <kalin_markoff@mail.bg>
2017-04-23 19:41:54 +0300

    Progr Fund - Strings and Text Processing

---
+++ Programming Fund Extended/14. Strings and Text Processing - Lab/14. Strings and Text Processing - Lab/04. Palindromes/Palindromes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Palindromes
+{
+    static void Main()
+    {
+        var words = Console.ReadLine().Split(new[] { ' ', ',', '.', '!', '?', ':' }, StringSplitOptions.RemoveEmptyEntries);
+        var palindromes = new SortedSet<string>();
+
+        foreach (var word in words)
+        {
+            var leftPart = word.Substring(0, word.Length / 2);
+            var startIndex = word.Length % 2 == 0 ? word.Length / 2 : word.Length / 2 + 1;
+            var rightPart = string.Join("", word.Substring(startIndex).ToCharArray().Reverse());
+
+            if (leftPart.Equals(rightPart))
+            {
+                palindromes.Add(word);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", palindromes));
+    }
+}
+
