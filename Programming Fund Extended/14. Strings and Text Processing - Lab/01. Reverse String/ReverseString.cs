f453d3bd6e692745fa2686e4ab011a8863c0b62d
Калин Марков <kalin_markoff@mail.bg>
2017-04-24 07:55:55 +0300

    change dir

---
+++ Programming Fund Extended/14. Strings and Text Processing - Lab/01. Reverse String/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class ReverseString
+{
+    static void Main()
+    {
+        var inputString = Console.ReadLine();
+        var reversedString = string.Join("", inputString.ToCharArray().Reverse());
+        Console.WriteLine(reversedString);
+    }
+}
+
