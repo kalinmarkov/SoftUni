db19a7d3ad1ad09c7dc15e450afc28497b93e840
Калин Марков <kalin_markoff@mail.bg>
2017-04-26 21:40:14 +0300

    string exercise

---
+++ Programming Fund Extended/16. Strings and Text Processing - More Ex/01. Serialize String/SerializeString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+        var charCounter = new Dictionary<char, HashSet<int>>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            var pattern = text[i];
+            if (!charCounter.ContainsKey(pattern))
+            {
+                charCounter[pattern] = new HashSet<int>();
+            }
+
+            var index = -1;
+
+            while (true)
+            {
+                index = text.IndexOf(pattern, index + 1);
+
+                if (index < 0)
+                {
+                    break;
+                }
+
+                charCounter[pattern].Add(index);
+            }
+
+        }
+
+        foreach (var item in charCounter)
+        {
+            var pattern = item.Key;
+            var indexOfPattern = string.Join("/", item.Value);
+            Console.WriteLine($"{pattern}:{indexOfPattern}");
+        }
+    }
+}
+
---
+++ Programming Fund Extended/16. Strings and Text Processing - More Ex/02. Stateless/Stateless.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Stateless
+{
+    static void Main()
+    {
+        // 60/100
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "collapse")
+            {
+                break;
+            }
+
+            var pattern = Console.ReadLine();
+            var symbol = string.Empty;
+
+            for (int i = 0; i < pattern.Length - 1; i++)
+            {
+                input = input.Replace(pattern, string.Empty).Trim();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (pattern[i] == input[j])
+                    {
+                        symbol = pattern[i].ToString();
+                        input = input.Replace(symbol, string.Empty).Trim();
+                    }
+
+                }
+
+            }
+
+            if (input.Length == 0)
+            {
+                Console.WriteLine("(void)");
+            }
+            else
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
+
