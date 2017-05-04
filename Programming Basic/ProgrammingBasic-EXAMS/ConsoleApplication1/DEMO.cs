f55eaeeb813bd689bf043f4017c9cfa054adf057
Калин Марков <kalin_markoff@mail.bg>
2017-05-04 22:14:06 +0300

    Add solutions

--- Programming Basic/ProgrammingBasic-EXAMS/ConsoleApplication1/DEMO.cs
+++ Programming Basic/ProgrammingBasic-EXAMS/ConsoleApplication1/DEMO.cs
@@ -1,10 +1,33 @@
 ﻿using System;
-using System.Globalization;
 
 class Program
 {
     private static void Main()
     {
-        
+        //  Програма, за сортиране на имена, по вече зададен масив от разбъркани имена
+
+        //  Създаваме масив от стрингове с готов набор от имена зададени в разбъркан ред
+        string[] names =
+            {
+                "Ivan",
+                "Ani",
+                "Georgi",
+                "Todor",
+                "Venci",
+                "Milen",
+                "Maria",
+                "Ivailo",
+                "Svetla",
+                "Petar"
+            };
+
+        //  Извикваме готова функция за масиви и сортираме масива по азбучен ред
+        Array.Sort(names);
+
+        //  Запазваме в променлива стринг, готовия за разпечатване масив. Всяко име на нов ред
+        string result = string.Join(Environment.NewLine, names);
+
+        //  Разпечатваме резултата
+        Console.WriteLine(result);
     }
 }
