08d0462200b07ac4e0df61c65d0603d531777f10
Калин Марков <kalin_markoff@mail.bg>
2017-05-14 17:31:23 +0300

    Edit solutions

--- Programming Fund Extended/18. EXAMS/18. EXAMS/21. Worm Ipsum/WormIpsum.cs
+++ Programming Fund Extended/18. EXAMS/18. EXAMS/21. Worm Ipsum/WormIpsum.cs
@@ -2,79 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 class WormIpsum
 {
     static void Main()
     {
+        var sentenceReg = new Regex(@"^[A-Z][^\.]*\.$");
+        var wordsReg = new Regex(@"[^\s,\.]+");
+
         while (true)
         {
-            var inputText = Console.ReadLine();
-            if (inputText == "Worm Ipsum")
+            var input = Console.ReadLine();
+
+            if (input == "Worm Ipsum")
             {
                 break;
             }
 
-            if (!inputText.EndsWith("."))
+            var sentenceMatch = sentenceReg.Match(input);
+
+            if (sentenceMatch.Success)
             {
-                continue;
-            }
-
-            inputText = inputText.Trim('.');
-
-            if (inputText.Contains("."))
-            {
-                continue;
-            }
-
-            var tokens = inputText.Split().ToList();
-
-            var firstWord = tokens.First();
-
-            if (!StartsWithUpper(firstWord))
-            {
-                continue;
-            }
-
-
-            for (int i = 0; i < tokens.Count; i++)
-            {
-                var hasRepeatedChar = false;
-                var repeatedChar = '\0';
-                var word = tokens[i];
-                for (int j = 0; j < word.Length; j++)
+                var wordsMatches = wordsReg.Matches(input);
+                foreach (Match match in wordsMatches)
                 {
-                    var count = 0;
-                    var currentChar = word[j];
-                    for (int k = 0; k < word.Length; k++)
+                    var currentWord = match.Value;
+                    if (currentWord.Length != currentWord.Distinct().Count())
                     {
-                        var checkedChar = word[k];
-                        if (checkedChar == currentChar)
-                        {
-                            count++;
-                            if (count > 1)
-                            {
-                                repeatedChar = currentChar;
-                                hasRepeatedChar = true;
-                            }
-                        }
+                        var symbol = currentWord.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+                        var newWord = string.Concat(Enumerable.Repeat(symbol, currentWord.Length));
+                        input = Regex.Replace(input, currentWord, newWord);
+
                     }
                 }
 
-                if (hasRepeatedChar)
-                {
-                    tokens[i] = word.Replace(word, new string(repeatedChar, word.Length));
-                }
-
+                Console.WriteLine(input);
             }
-
-            Console.WriteLine($"{string.Join(" ", tokens)}.");
+            
         }
     }
-    public static bool StartsWithUpper(string word)
-    {
-        return Enumerable.Range(65, 26).Contains((int)word[0]);
-    }
+    
 }
 
