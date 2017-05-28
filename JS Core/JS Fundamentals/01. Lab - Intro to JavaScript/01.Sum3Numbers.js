3a7a538316a93f68e396f2c0d1cd94ad1f14187a
Калин Марков <kalin_markoff@mail.bg>
2017-05-28 10:12:32 +0300

    Change dir name

---
+++ JS Core/JS Fundamentals/01. Lab - Intro to JavaScript/01.Sum3Numbers.js
@@ -0,0 +1,9 @@
+function sumNumbers (a ,b, c) {
+    let num1 = Number(a);
+    let num2 = Number(b);
+    let num3 = Number(c);
+    let sum = num1 + num2 + num3;
+    console.log(sum);
+}
+
+sumNumbers(2, 3, 4);
