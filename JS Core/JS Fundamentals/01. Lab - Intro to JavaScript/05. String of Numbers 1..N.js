4cec35435cce4f514085467c4ae258c6b4c87931
Калин Марков <kalin_markoff@mail.bg>
2017-06-13 18:33:03 +0300

    Modify solutions

--- JS Core/JS Fundamentals/01. Lab - Intro to JavaScript/05. String of Numbers 1..N.js
+++ JS Core/JS Fundamentals/01. Lab - Intro to JavaScript/05. String of Numbers 1..N.js
@@ -1,10 +1,10 @@
 function numbers(n) {
     let num = Number(n);
     let str = '';
-    for (let i=1; i<=num; i++) {
+    for (let i=1; i<= num; i++) {
         str += i;
     }
     return str;
 }
 
-numbers(['11']);
+numbers('11');
